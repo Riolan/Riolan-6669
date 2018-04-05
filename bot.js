457ba13ced5cb966a42b9712da7284dcf80e5da3
Riolan <riolaney@att.net>
2018-04-04 22:07:41 -0500

    updated role functions

--- bot.js
+++ bot.js
@@ -1,35 +1,55 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
 var baseList = require('./naughtywords.json');
 var prefix = "~";
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setGame('[help] for help!');
+   console.log(`Logged in as ${client.user.tag}!`);
+   client.user.setActivity(" ~help || twitch.tv/x_rio");
+   console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 });
 
-
-client.on('message', msg => {
-  if (msg.content === '[help]') {
-    msg.reply('``` List of commands \n ------- \n Commands: \n (1.) [help] - Brings up this text. \n (2.) Please use [add] (if staf member) or go to a role select channel.```');
-  }
-});
-
-
-client.on('message', msg => {
-  if (msg.content === "Rio") {
-    if ( Math.floor(Math.random() * 20) > 10) {
-      msg.reply("Leaf me alone.")
-    } else {
-      msg.reply("What is it?")
+var check = [];
+async function checkForSpam(msg) {
+    var msgTime = msg.createdTimestamp
+    var msgUser = msg.member.user.id
+    check.push(msgUser)
+    if (check.length % 6 === 0) {
+      for (var i = 0; i < 6; i++) {
+        if (msgUser === check[0] && msgUser === check[1] && msgUser === check[2] && msgUser === check[3] && msgUser === check[4] && msgUser === check[5]) {
+          if (i === 3) {
+            msg.reply("Please don't spam {warning}")
+          }
+        }
+      }
+      check = [];
     }
   }
-})
+
+  var baseList = require('./naughtywords.json');
+  async function checkForProfanity(msg) {
+    var message = msg.content.toLowerCase()
+    messageSliced = message.trim().split(/ +/g)
+    var profanity = baseList.words
+    for(var i = 0; i < messageSliced.length; i++) {
+      for(var j = 0; j < profanity.length; j++) {
+        if (profanity[j] === messageSliced[i]) {
+          msg.delete()
+          .catch(console.error)
+          await msg.channel.send("pls no curse-y curse-y", {
+            file:"https://media.discordapp.net/attachments/428020896251510784/430886269313220618/0b3f9a7adf4e54addbf6543de0798e8b.png"
+          })
+        }
+      }
+    }
+  }
+
+
+const newUsers = [];
 
 client.on("message", async msg => {
-//  checkForSpam(msg)
-  checkForProfacity(msg)
+  checkForSpam(msg)
+  checkForProfanity(msg)
   if(msg.author.bot) return;
   if(msg.content.indexOf(prefix) !== 0) return;
   const args = msg.content.slice(prefix.length).trim().split(/ +/g);
@@ -37,26 +57,29 @@ client.on("message", async msg => {
   if (command === "ping") {
     msg.channel.send(":ping_pong: Pong")
   }
-
 })
 
-async function checkForProfacity(msg) {
-  var message = msg.content.toLowerCase()
-  messageSliced = message.trim().split(/ +/g)
-  var profanity = baseList.words
-  for(var i = 0; i < messageSliced.length; i++) {
-    for(var j = 0; j < profanity.length; j++) {
-      if (profanity[j] === messageSliced[i]) {
-        msg.delete()
-        .catch(console.error)
-        await msg.channel.send("BEGONE THOT", {
-          file:"https://media.discordapp.net/attachments/428020896251510784/430886269313220618/0b3f9a7adf4e54addbf6543de0798e8b.png"
-        })
-      }
+client.on("guildMemberAdd", (member) => {
+    const defaultChannel = member.guild.channels.find(c=> c.name === "new-members" && c.type === "text");
+    function roleFunction() {
+      var role  = member.guild.roles.find("name", "Member")
+      defaultChannel.send("Welcome to our music group, " + member + " enjoy your time!");
+      member.addRole(role)
+      .catch(console.error)
     }
-  }
-}
-
+    if (!defaultChannel) {
+      var server = member.guild;
+      var name = "new-members";
+      server.createChannel(name, "text", [{
+        type: "role",
+        id: "1"
+      }])
+      .then(roleFunction())
+      .catch(console.error)
+    } else {
+      roleFunction()
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN)
